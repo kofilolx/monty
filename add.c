67e836a755cd923db8383f0541cc037bfb848f12
jonathan <kofyamando@gmail.com>
2024-01-18 22:24:49 +0000

    monty :betty

--- add.c
+++ add.c
@@ -11,31 +11,30 @@ void m_add(stack_t **head, unsigned int line_counter)
 {
 	stack_t *h;
 	int len;
-    int tmp;
+	int tmp;
 
-    len  = 0;
+	len  = 0;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	
-    if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_counter);
-		fclose(bus.file);
-		free(bus.content);
-		
-        free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	
     h = *head;
-	tmp = h->n + h->next->n;
-	
+    while (h)
+    {
+        h = h->next;
+        len++;
+    }
+
+    if (len < 2)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+
+        exit(EXIT_FAILURE);
+    }
+
+    h = *head;
+    tmp = h->n + h->next->n;
     h->next->n = tmp;
-	*head = h->next;
-	free(h);
+    *head = h->next;
+    free(h);
 }
