b6d6eca1d454178e474066f5679239d6135b3798
kofilolx <kofyamando@gmail.com>
2024-01-19 02:25:42 -0800

    monty merge file

--- add.c
+++ add.c
@@ -12,7 +12,7 @@ void m_add(stack_t **head, unsigned int line_counter)
 	stack_t *h;
 	int len;
 	int tmp;
-	
+
 	len  = 0;
 	h = *head;
 	while (h)
@@ -33,4 +33,31 @@ void m_add(stack_t **head, unsigned int line_counter)
 	h->next->n = tmp;
 	*head = h->next;
 	free(h);
+}
+
+
+/**
+ * add_node - add node to the head stack
+ * @head: head of the stack
+ * @n: new_value
+ * Return: no return
+*/
+
+void add_node(stack_t **head, int n)
+{
+	stack_t *new_node, *tmp;
+	
+	tmp = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	if (tmp)
+		tmp->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
 }
--- execute.c
+++ execute.c
@@ -47,3 +47,21 @@ int execute(char *content, stack_t **stack, unsigned int line_counter, FILE *fil
     }
     return (1);
 }
+
+/**
+* free_stack - frees a doubly linked list
+* @head: head of the stack
+*/
+
+void free_stack(stack_t *head)
+{
+	stack_t *tmp;
+	
+	tmp = head;
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
